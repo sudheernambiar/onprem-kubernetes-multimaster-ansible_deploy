e2cfbf05f0ebddfa1539dfd6aaba1aa77ae8d2e4
sudheer <sudheer.ck@outlook.com>
2022-05-05 04:53:01 +0400

    dev-v2

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Alpha
-ansible-playbook -i hosts k-alpha.yml -e 'ansible_user=root ansible_password=PrIsa221S476e#'
+ansible-playbook -i hosts k-alpha.yml -e 'ansible_user=root ansible_password=root_password'
 
 #Masters
-ansible-playbook -i '172.30.17.62', -e 'host_name=k8s-master-2 p_value=254 ansible_user=root ansible_password=PrIsa221S476e#' k-master.yml
-ansible-playbook -i '172.30.17.63', -e 'host_name=k8s-master-3 p_value=253 ansible_user=root ansible_password=PrIsa221S476e#' k-master.yml
+ansible-playbook -i '192.168.29.32', -e 'host_name=k8s-master-2 p_value=254 ansible_user=root ansible_password=root_password' k-master.yml
+ansible-playbook -i '192.168.29.33', -e 'host_name=k8s-master-3 p_value=253 ansible_user=root ansible_password=root_password' k-master.yml
 
 #Slave
-ansible-playbook -i '172.30.17.64', -e 'host_name=k8s-node-1 ansible_user=root ansible_password=PrIsa221S476e#' k-slave.yml
-ansible-playbook -i '172.30.17.65', -e 'host_name=k8s-node-2 ansible_user=root ansible_password=PrIsa221S476e#' k-slave.yml
+ansible-playbook -i '192.168.29.34', -e 'host_name=k8s-node-1 ansible_user=root ansible_password=root_password' k-slave.yml
+ansible-playbook -i '192.168.29.35', -e 'host_name=k8s-node-2 ansible_user=root ansible_password=root_password' k-slave.yml
